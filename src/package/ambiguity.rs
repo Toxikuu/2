5f4b4438e8adcda8c3cac55b7556c3edcada012a
Toxikuu <toxikuu@users.noreply.github.com>
2024-12-30 17:36:28 -0600

    refactoring and ignore hidden directories in locate()

--- src/package/ambiguity.rs
+++ src/package/ambiguity.rs
@@ -3,17 +3,30 @@
 // searches for a package among all the installed repos, prompting when extant in multiple repos
 
 use walkdir::WalkDir;
-use crate::{pr, die, select};
+use crate::{pr, die, select, erm};
 
 fn locate(name: &str) -> Vec<String> {
     let mut matches = Vec::new();
 
-    for entry in WalkDir::new("/usr/ports").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new("/usr/ports")
+        .into_iter()
+        .flatten()
+        .filter(|e| {
+            !e.path()
+            .components()
+            .any(|component| component.as_os_str().to_string_lossy().starts_with('.'))
+        })
+    {
         if entry.file_type().is_dir() {
-            if let Some(filename) = entry.file_name().to_str() {
-                if filename == name {
-                    matches.push(entry.path().to_string_lossy().strip_prefix("/usr/ports/").unwrap().to_string())
-                }
+            let filename = entry.file_name().to_string_lossy();
+            if filename == name {
+                matches.push(
+                    entry.path()
+                        .to_string_lossy()
+                        .strip_prefix("/usr/ports/")
+                        .unwrap()
+                        .to_string()
+                )
             }
         }
     }
@@ -25,8 +38,9 @@ pub fn resolve_ambiguity(name: &str) -> String {
     let matches = locate(name);
 
     if matches.is_empty() { die!("Failed to find '{}' in any repo", name) }
-    if matches.len() == 1 { return matches.first().unwrap().to_string() }
+    if let [only] = matches.as_slice() { return only.to_string() }
 
+    erm!("Ambiguous: '{}'", name);
     for (i, m) in matches.iter().enumerate() {
         pr!("{}. {}", i, m)
     }
