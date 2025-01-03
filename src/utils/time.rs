87d407f0997acc61a5e71294479f0d502452f7c4
Toxikuu <toxikuu@users.noreply.github.com>
2025-01-03 00:57:28 -0600

    documentation, display(), pretty() added

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -4,6 +4,9 @@
 
 use std::time::{Duration, Instant};
 
+/// # Description
+/// Stopwatch is a helper struct for timing sections of code
+/// It's mostly used for PM endpoints
 pub struct Stopwatch {
     pub start_time: Option<Instant>,
     elapsed: Duration,
@@ -17,12 +20,16 @@ impl Stopwatch {
         }
     }
 
+    /// # Description
+    /// Starts (or resumes) the stopwatch
     pub fn start(&mut self) {
         if self.start_time.is_none() {
             self.start_time = Some(Instant::now());
         }
     }
 
+    /// # Description
+    /// Stops (pauses) the stopwatch
     pub fn stop(&mut self) {
         if let Some(start_time) = self.start_time {
             self.elapsed += start_time.elapsed();
@@ -30,16 +37,60 @@ impl Stopwatch {
         }
     }
 
+    /// # Description
+    /// Resets the stopwatch
     pub fn reset(&mut self) {
         self.start_time = None;
         self.elapsed = Duration::ZERO;
     }
 
+    /// # Description
+    /// Returns the total elapsed time
     pub fn elapsed(&self) -> Duration {
         if let Some(start_time) = self.start_time {
             self.elapsed + start_time.elapsed()
         } else {
             self.elapsed
+        }
+    }
+
+    /// # Description
+    /// Displays the elapsed time in a human-readable format
+    pub fn display(&self) -> String {
+        self.elapsed().pretty()
+    }
+}
+
+/// # Description
+/// Adds the pretty() method for Duration
+/// This is used by Stopwatch::display()
+trait PrettyDuration {
+    fn pretty(self) -> String;
+}
+
+impl PrettyDuration for Duration {
+    /// # Description
+    /// Displays duration in a human-readable, dare I say pretty, format
+    fn pretty(self) -> String {
+        let total_millis = self.as_millis_f32();
+        if total_millis < 1. {
+            format!("{} ns", self.as_nanos())
+        } else if total_millis < 1_000. {
+            format!("{:.3} ms", total_millis)
+        } else if total_millis < 60_000. {
+            format!("{:.3} s", self.as_secs_f32())
+        } else {
+            let total_secs = self.as_secs();
+            if total_secs < 3_600 {
+                format!("{}:{:02} min", total_secs / 60, total_secs % 60)
+            } else {
+                format!(
+                    "{}:{:02}:{:02} hrs",
+                    total_secs / 3_600,
+                    total_secs % 3_600 / 60,
+                    total_secs % 60,
+                )
+            }
         }
     }
 }
