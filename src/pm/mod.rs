874b9018bd5b61ed2e989b540388363f432f713b
Toxikuu <toxikuu@users.noreply.github.com>
2025-01-31 18:34:59 -0600

    added parallelism support for pm

--- src/pm/mod.rs
+++ src/pm/mod.rs
@@ -5,16 +5,35 @@ pub mod endpoints;
 
 use crate::package::Package;
 
+#[cfg(feature = "parallelism")]
+pub use {
+    crate::utils::par::build_pool,
+    rayon::ThreadPool,
+};
+
 /// # Description
 /// The package manager struct
 pub struct PM<'a> {
-    pub packages: &'a [Package]
+    pub packages: &'a [Package],
+    #[cfg(feature = "parallelism")]
+    pub thread_pool: ThreadPool,
 }
 
 impl<'a> PM<'a> {
     /// # Description
     /// Creates a new package manager struct from an array of packages
+    #[cfg(not(feature = "parallelism"))]
     pub const fn new(packages: &'a [Package]) -> Self {
         Self { packages }
+    }
+
+    /// # Description
+    /// Creates a new package manager struct from an array of packages
+    #[cfg(feature = "parallelism")]
+    pub fn new(packages: &'a [Package]) -> Self {
+        Self { 
+            packages,
+            thread_pool: build_pool(packages),
+        }
     }
 }
