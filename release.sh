c2703855f57ee279b9d1d32b4bee18a92b2fc703
Toxikuu <toxikuu@users.noreply.github.com>
2025-02-16 05:21:12 -0600

    more improvements to release.sh

--- release.sh
+++ release.sh
@@ -17,6 +17,10 @@ info() {
   echo -e "\x1b[36;1m [INFO] $1\x1b[0m"
 }
 
+good() {
+  echo -e "\x1b[32;1m [GOOD] $1\x1b[0m"
+}
+
 fail() {
   echo -e "\x1b[31;1m [FAIL] ${1:-Unknown failure}\x1b[0m"
   exit "${2:-1}"
@@ -25,38 +29,49 @@ fail() {
 pushd . > /dev/null
 cd "$SCRIPT_DIR"
 
+info 'Pushing...'
+git push
+good 'Pushed'
+
 info 'Ensuring dependencies are up to date...'
 rustup override set nightly
-out=$(cargo update | tee /dev/tty)
+out=$(cargo update 2>&1 | tee /dev/tty)
 if ! echo "$out" | grep -q 'Locking 0 packages to'; then
   git add Cargo.lock
-  git commit -m "updated dependencies"
+  git commit -m 'updated dependencies'
   git push
+  good 'Automatically updated dependencies'
 fi
+good 'Dependency versions passing'
 
 info 'Ensuring all changes were committed...'
 if [ "$(git status -s | wc -l)" -ne 0 ]; then
   fail 'Some changes were not committed'
 fi
+good 'Git status passing'
 
 info 'Checking for unused dependencies...'
 out=$(cargo udeps | tee /dev/tty)
 if ! echo "$out" | grep -q 'All deps seem to have been used.'; then
   fail 'Unused dependencies detected'
 fi
+good 'Dependency use passing'
 
 info 'Checking for security vulnerabilities...'
 out=$(cargo audit | tee /dev/tty)
 if echo "$out" | grep -q 'Vulnerable crates found!'; then
   fail 'Failed security audit'
 fi
+good 'Security audit passing'
 
 info 'Running tests...'
 cargo nextest run || fail 'Some tests failed'
+good 'Tests passing'
 
 info 'Building the release binary...'
 cargo build --release
 cargo strip || fail 'Failed to strip binary. Are you missing cargo-strip?'
+good 'Built and stripped the binary'
 
 info 'Organizing the release...'
 rm -rf release
@@ -65,11 +80,13 @@ mkdir -v release
 for i in "target/release/two" "Cargo.lock"; do
   cp -v "$i" release
 done
+good 'Release made'
 
 info 'Validating all scripts...'
 ( find . -type f -name '*.sh' -print0
   find scripts -type f -print0
 ) | xargs -0 shellcheck -s bash || warn 'Scripts failed validation'
+good 'Scripts passing'
 
 popd > /dev/null
 info 'Done!'
